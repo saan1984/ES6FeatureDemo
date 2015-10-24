0cc53dc87d77539f4a0f1575ac7d17604f5ba0b6
saan1984 <sandeep_giet@yahoo.com>
2015-10-24 20:26:42 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel');
 
-
-
 gulp.task('babel', function () {
     return gulp.src('src/**/*.js')
         .pipe(babel({optional: ['runtime']}))
